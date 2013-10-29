111f034800f0c4fb4e9aa63e5e3495e45ec27b61
David M. Lee <dlee@digium.com>
2013-10-29 09:22:00 -0500

    More differences between versions

--- publish.sh
+++ publish.sh
@@ -137,30 +137,44 @@ fi
 #
 case ${BRANCH_NAME} in
     1.8|10*)
-        # Asterisk 1.8 docs don't have version number in the prefix
-        PREFIX_ARG=""
-        # make full introduced in 11
-        make all
+        # 10 and earlier only had core docs
+        make doc/core-en_US.xml
+        mv -f doc/core-en_US.xml ${TOPDIR}/asterisk-docs.xml
+        ;;
+    11)
+        # 11 had full docs
+        make doc/full-en_US.xml
+        mv -f doc/full-en_US.xml ${TOPDIR}/asterisk-docs.xml
         ;;
     *)
-        PREFIX_ARG="--prefix=\"Asterisk ${BRANCH_NAME}\""
-        make full
+        # 12 and later needs to run Asterisk, so a full build
+        # is necessary
+        make full install samples
+
+        killall -9 asterisk || true # || true so set -e doesn't kill us
+        ${AST_DIR}/sbin/asterisk
+
+        rm -f ${TOPDIR}/full-en_US.xml
+        ${AST_DIR}/sbin/asterisk -x "xmldoc dump ${TOPDIR}/asterisk-docs.xml"
+
+        # Kill Asterisk, and wait for it to die
+        AST_PID=$(cat ${AST_DIR}/var/run/asterisk/asterisk.pid)
+        killall -9 asterisk || true # || true so set -e doesn't kill us
+        while kill -0 ${AST_PID}; do
+            sleep 0.1
+        done
         ;;
 esac
-make install samples
 
-killall -9 asterisk || true # Or true so set -e doesn't kill us
-${AST_DIR}/sbin/asterisk
-
-rm -f ${TOPDIR}/full-en_US.xml
-${AST_DIR}/sbin/asterisk -x "xmldoc dump ${TOPDIR}/full-en_US.xml"
-
-# Kill Asterisk, and wait for it to die
-AST_PID=$(cat ${AST_DIR}/var/run/asterisk/asterisk.pid)
-killall -9 asterisk || true # Or true so set -e doesn't kill us
-while kill -0 ${AST_PID}; do
-    sleep 0.1
-done
+#
+# Set the prefix argument for publishing docs
+#
+if test ${BRANCH_NAME} = 1.8; then
+        # Asterisk 1.8 docs don't have a prefix
+        PREFIX=""
+else
+        PREFIX="Asterisk ${BRANCH_NAME}"
+fi
 
 #
 # Publish XML documentation.
@@ -171,7 +185,7 @@ cd ${TOPDIR}
 
 ${TOPDIR}/astxml2wiki.py --username="${CONFLUENCE_USER}" \
     --server=${CONFLUENCE_URL} \
-    ${PREFIX_ARG} \
+    --prefix="${PREFIX}" \
     --space="${CONFLUENCE_SPACE}" \
-    --file=${TOPDIR}/full-en_US.xml \
+    --file=${TOPDIR}/asterisk-docs.xml \
     --debug -v
